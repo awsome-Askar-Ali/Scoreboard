a5cf62f628fd6694525d7957e4e6253adb3cdf97
Misbha <misbhafarheen98@gmail.com>
2023-01-08 14:39:30 +0530

    Completed: Stories setup for test the components

---
+++ src/core/heading.jsx
@@ -0,0 +1,10 @@
+// Core - Heading
+import React from 'react';
+
+const Heading = ({ title }) => {
+    return (
+        <h1>{title}</h1>
+    )
+};
+
+export default Heading;
---
+++ src/stories/core/heading.stories.jsx
@@ -0,0 +1,11 @@
+// Core - Heading
+import React from 'react';
+
+import Heading from '../../core/heading';
+
+export default {
+  title: 'Heading',
+  component: Heading,
+};
+
+export const H1 = () => <Heading title="Test heading" />;
